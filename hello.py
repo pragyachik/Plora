a2bdc8be3bca3bef6edaae4fc23d613e1b352ab4
Pragya Agrawal <pragyachik@gmail.com>
2023-06-24 17:02:56 +0530

    test response

--- hello.py
+++ hello.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/returnjson', methods = ['GET'])
+def ReturnJSON():
+    if(request.method == 'GET'):
+        data = {
+            "text" : "Hello, World!",
+        }
+
+        if data:
+            with open('data.json', 'w') as json_file:
+                json.dump(data, json_file)  # Write the JSON data to the file
+
+        response = jsonify(data)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+
+if __name__ == '__main__':
+    app.run()
