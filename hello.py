3c387f3f8bd2ddced0ebce6d4453a98ff561c31f
Pragya Agrawal <pragyachik@gmail.com>
2023-06-29 16:42:24 +0530

    token api

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 import json
 import random
+import datetime
 # from flask_cors import CORS
+import os
 
 
 app = Flask(__name__)
@@ -11,16 +13,66 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/api/gettoken", methods = ["POST"])
+@app.route("/api/getRedPajamaToken", methods = ["POST"])
 def get_token():
     data = json.loads(request.data)
     print(data)
     token = random.randint(0,10000)
-    with open(str(token)+'.json', 'w') as f:
+    created_at = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    token_item = {
+            "token":str(token),
+            "status":"Not Processed",
+            "createdAt":str(created_at)
+        }
+    
+    # Check if the directory exists
+    if not os.path.exists('./tokens/'):
+        os.makedirs('./tokens/')
+
+    with open('tokens/'+str(token)+'.json', 'w') as f:
         f.write(json.dumps(data, indent=2))
+    
+    # Define the path to your JSON file
+    json_file_path = "./allTokens.json"
+
+    # Check if the JSON file exists
+    if not os.path.exists(json_file_path):
+        # Create an empty JSON file
+        with open(json_file_path, "w") as json_file:
+            json.dump([], json_file)
+
+    with open(json_file_path, 'r') as f:
+        allTokensData = json.load(f)
+
+    allTokensData.append(token_item)
+
+    with open(json_file_path, 'w') as f:
+        f.write(json.dumps(allTokensData, indent=2))
     
 
     response = jsonify({"token":token})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/api/getTokenOutput', methods = ['POST'])
+def get_token_output():
+    data = json.loads(request.data)
+    print(data)
+    token = data['token']
+    responseData = {
+        "message": "error"
+    }
+    token_output_path = './tokenOutputs/'+str(token)+'.json'
+    if os.path.exists(token_output_path):
+        with open(token_output_path, 'r') as f:
+            responseData = json.load(f)
+    else:
+        responseData = {
+            "message": "token not processed"
+        }
+
+    response = jsonify(responseData)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
